24d2e284c322bfca5389c765f3bfa870ee39cb00
Chris Parsonson <cjparsonson@googlemail.com>
2023-05-14 17:34:20 +0100

    Add project files.

---
+++ PrimeFactorsLib/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace PrimeFactorsLib
+{
+    public class Primes
+    {
+        public static readonly int[] PrimeNumbers = new[]
+        {
+            19, 17, 13, 11,
+            7, 5, 3, 2
+        };
+
+        public static string PrimeFactors(int number)
+        {
+            if ((number < 1) || (number > 1000)) // Test for initial validity
+            {
+                return $"{nameof(number)} must be between 1 and 1000";
+            }
+
+            string factors = string.Empty; // Initalize empty string this will be returned.
+
+            foreach (int divisor in PrimeNumbers)
+            {
+                int remainder; // Initalize empty int to hold remainder
+                do
+                {
+                    remainder = number % divisor; // Will be 0 if divisble by this prime
+                    if (remainder == 0)
+                    {
+                        number /= divisor;
+                        if (number == 1) // Signifies this is the last primte factor
+                        {
+                            factors += $"{divisor}";
+                        }
+                        else
+                        {
+                            factors += $"{divisor} x "; // More prime factors to come, prepare string accordingly
+                        }
+                    }
+
+                } while (remainder == 0);
+            }
+            return factors;
+        }
+    }
+}
---
+++ PrimeFactorsTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PrimeFactorsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
